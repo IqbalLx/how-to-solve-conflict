a6c22bea7628c629462f07810781f1ccde1ad3d9
Arif Widianto <arifjozz123@gmail.com>
2023-12-16 22:43:41 +0700

    membuat implementasi sort pada main.c

--- main.c
+++ main.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
-
+#include "sorting.h"
 #include "searching.h"
+
+void display_list(int *list, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d ", list[i]);
+    }
+}
 
 int main()
 {
-    int arr[3] = {
+    int arr[5] = {
         1,
-        2,
         3,
+        4,
+        2,
+        5,
     };
+
+    printf("\n");
+    printf("Original Array : ");
+    display_list(arr, 5);
+
+    sort(arr, 5, 0);
+    printf("\n");
+    printf("Ascending Sort Result : ");
+    display_list(arr, 5);
 
     int search_index = search(arr, 2, 3);
     if (search_index != -1)
